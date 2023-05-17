f16f5423daed221e484d2e1752a42c109c9fe98c
Nakakandebr <bridgetnakakandea@gmail.com>
2023-05-17 21:32:02 +0300

    Classifies products to their categories

--- products.py
+++ products.py
@@ -1,21 +1,18 @@
-class Product:
-    def __init__(self, name, price, quantity):
+class ProductCategory:
+    def __init__(self, name, description):
         self.name = name
-        self.price = price
-        self.quantity = quantity
-
-class Cart:
-    def __init__(self):
+        self.description = description
         self.products = []
 
     def add_product(self, product):
         self.products.append(product)
 
-    def remove_product(self, product):
-        self.products.remove(product)
-
-    def get_total_price(self):
-        total_price = 0
+    def display_products(self):
+        print(f"Category: {self.name} - {self.description}")
         for product in self.products:
-            total_price += product.price * product.quantity
-        return total_price
+            print(f"{product.name} - {product.description}")
+            
+class Product:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
