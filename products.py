e26904fb875a79d035b06a09fff9a3e60f8bb41d
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-18 13:42:22 +0300

    Creates the product category feature

--- products.py
+++ products.py
@@ -4,12 +4,13 @@ class ProductCategory:
         self.description = description
         self.price = price
         self.products = []
+        
     def add_product(self, product):
         self.products.append(product)
 
     def remove_product(self, product):
         self.products.remove(product)
-        
+
     def display_products(self):
         for product in self.products:
             print(product.name, product.description, product.price)
