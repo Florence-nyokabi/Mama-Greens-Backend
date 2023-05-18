4b0be19a85b3c7bd236ff1da03f3efebd7b56122
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-18 13:41:50 +0300

    User registration

--- products.py
+++ products.py
@@ -1,18 +1,15 @@
 class ProductCategory:
-    def __init__(self, name, description):
+    def __init__(self, name, description, price):
         self.name = name
         self.description = description
+        self.price = price
         self.products = []
-
     def add_product(self, product):
         self.products.append(product)
 
+    def remove_product(self, product):
+        self.products.remove(product)
+        
     def display_products(self):
-        print(f"Category: {self.name} - {self.description}")
         for product in self.products:
-            print(f"{product.name} - {product.description}")
-            
-class Product:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+            print(product.name, product.description, product.price)
