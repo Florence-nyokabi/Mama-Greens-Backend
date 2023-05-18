c20d1603097f9b9512683eab66f822366b94cde5
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-18 13:00:08 +0300

    User registration

---
+++ user-registration.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, name, password, email):
+        self.name = name
+        self.password = password
+        self.email = email
+    def register(self):
+        new_user = User(self,name,password,email)
+        return new_user
+    def login(self, email, password):
+        if self.email == email and self.password == password:
+            print("Login successful!")
+        else:
+            print("Invalid login credentials")
